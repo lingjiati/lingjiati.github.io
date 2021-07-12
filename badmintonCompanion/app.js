2bcadba23faef50d17cc0786242d312aad515b9e
lingjiati <lingjiati@gmail.com>
2021-07-12 23:07:25 +0800

    Add files via upload

--- badmintonCompanion/app.js
+++ badmintonCompanion/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-	navigator.serviceWorker.register('sw.js')
+	navigator.serviceWorker.register('/sw.js')
 		.then(() => console.log('service worker registered'))
 		.catch((er) => console.log('servide worker not registered', er))
 }
