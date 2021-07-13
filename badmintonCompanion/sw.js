2448d99e219d34c23edc1b65265fe7a2ea356ff2
lingjiati <lingjiati@gmail.com>
2021-07-13 17:15:35 +0800

    Add files via upload

--- badmintonCompanion/sw.js
+++ badmintonCompanion/sw.js
@@ -1,11 +1,11 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/css/w3.css',
-  '/js/main.js',
-  '/js/Eligo.js',
+  '/badmintonCompanion',
+  'index.html',
+  'css/style.css',
+  'css/w3.css',
+  'js/main.js',
+  'js/Eligo.js',
   'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600&display=swap',
   'https://fonts.gstatic.com/s/materialicons/v92/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
   'https://fonts.gstatic.com/s/robotomono/v13/L0xTDF4xlVMF-BfR8bXMIhJHg45mwgGEFl0_3vrtSM1J-gEPT5Ese6hmHSh0mQ.woff2',
