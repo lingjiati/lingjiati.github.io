9799b0121b095c92f5b02d7338929b912d36e28b
lingjiati <lingjiati@gmail.com>
2021-08-29 11:15:50 +0800

    test

--- matchups/js/Sortable.js
+++ matchups/js/Sortable.js
@@ -4,7 +4,7 @@
  * @author	owenm    <owen23355@gmail.com>
  * @license MIT
  */
-(function (global, factory) {
+ (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.Sortable = factory());
